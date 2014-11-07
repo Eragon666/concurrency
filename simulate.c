b3c3c13808fac190d9d71838bea8809a665e3d92
Mehmet Faruk Ünal <10266232@acheron.ic.uva.nl>
2014-11-07 20:05:55 +0100

    finished simulate

--- simulate.c
+++ simulate.c
@@ -23,13 +23,13 @@ typedef struct order {
     int semaphore;
     int boole;
     int size;
-};
+} order;
 
 typedef struct list { 
     int nt;
     int start;
-    struct order arrays;
-};
+    struct order* arrays;
+} list;
 
 /* Add any functions you may need (like a worker) here. */
 void update_rotation(double **old, double **current, double **next){
@@ -39,46 +39,61 @@ void update_rotation(double **old, double **current, double **next){
   *next = temp;
 }
 
+void update_rota(struct order* tarray){
+  double *temp = tarray->old;
+  tarray->old = tarray->current;
+  tarray->current = tarray->next;
+  tarray->next = temp;
+}
+
 /* temp1 = (2 * current_array[i]) - old_array[i];
  * temp2 = 0.15 * (current_array[i-1] - (2 * current_array[i] - current_array[i+1]));
  * next_array[i] = temp1 + temp2;
  */
 
-void *compute(void *arguments)
+void *Compute(void *arguments)
 {
-    struct list *args;
-    args = (struct list *) arguments    
-    
+	struct list *args;
+    args = (struct list *) arguments ;
     int i;   
-    while(args->arrays->boole)
+    double temp1, temp2;
+	
+	
+	while(args->arrays->boole)
     { 
         
         for(i = args->start; i < args->arrays->size-1; i += args->nt) 
         {
-            temp1 = (2 * current_array[i]) - old_array[i];
-            temp2 = 0.15 * (current_array[i-1] - (2 * current_array[i] - current_array[i+1]));
-            next_array[i] = temp1 + temp2;  
+            temp1 = (2 * args->arrays->current[i]) - args->arrays->old[i];
+            temp2 = 0.15 * (args->arrays->current[i-1] - (2 * args->arrays->current[i] - args->arrays->current[i+1]));
+            args->arrays->next[i] = temp1 + temp2; 
+			
         }
         
-        pthread_mutex_lock(&count_mutex)
-        if(args->arrays->semaphore > 1)
+		
+        pthread_mutex_lock(&count_mutex);
+        //printf("%d\n", args->arrays->semaphore);
+		if(args->arrays->semaphore > 1)
         { 
-            args->arrays->semaphore = args->order->semaphore-1;
+			//printf("Hello5a\n");
+            args->arrays->semaphore = args->arrays->semaphore-1;
             pthread_cond_wait(&start_line ,&count_mutex);
         } 
         else if (args->arrays->semaphore == 1)
         { 
+		    //printf("Hello5b\n");
             args->arrays->semaphore = args->nt;
             pthread_cond_signal(&swap_arrays);
             pthread_cond_wait(&start_line ,&count_mutex);
             
         }
-        pthread_mutex_unlock(&count_mutex)
-        
+		//printf("Hello7\n");
+        pthread_mutex_unlock(&count_mutex);
+        pthread_cond_signal(&start_line);
         
     }
 }
-
+//0.944218 / 0.944219 / 0.9944207
 /*
  * Executes the entire simulation.
  *
@@ -95,61 +110,72 @@ double *simulate(const int i_max, const int t_max, const int num_threads,
         double *old_array, double *current_array, double *next_array)
 {
     
-    double temp1;
-    double temp2;
-    int tc;
+	int tc;
     struct list tlist[num_threads];
-    struct order tarray;
-
-    tarray.old = *old_array;
-    tarray.current = *current_array;
-    tarray.next = *next_array
-    tarray.semaphore = 0;
-    tarray.boole = 1;
-    tarray.size = i_max;
+    struct order *tarray = malloc (sizeof (struct order));
+    
+    tarray->old = old_array;
+    tarray->current = current_array;
+    tarray->next = next_array;
+    tarray->semaphore = num_threads;
+    tarray->boole = 1;
+    tarray->size = i_max;
     
     for (tc = 0; tc < num_threads; tc++)
     {
         tlist[tc].nt = num_threads;
         tlist[tc].start = tc + 1;
-        tlist[tc].arrays = &tarray; 
+        tlist[tc].arrays = tarray; 
     }
 
     pthread_t thread_ids[num_threads];
-    pthread_mutex_lock( &count_mutex);
+    //pthread_mutex_lock( &count_mutex);
     
+	
     for (tc = 0; tc < num_threads; tc++)
     {
         pthread_create( &thread_ids[tc],
                         NULL,
                         &Compute,
-                        NULL);               
+                        &tlist[tc]);               
 
     }
-    int i;
-    int count;    
+    int count; 
     
-    
+	
     for(count = 0; count < t_max; count++ )   
     {
-        //pthread_mutex_lock( &count_mutex);
+        pthread_mutex_lock( &count_mutex);
         pthread_cond_wait( &swap_arrays, &count_mutex);
          
 
-        // printf("%d:  %f - %f - %f\n", count, old_array[50000], current_array[50000], next_array[50000]);
+        //printf("1 %d:  %f - %f - %f\n", count, old_array[50000], current_array[50000], next_array[50000]);
+		//printf("2 %d:  %f - %f - %f\n", count, tarray->old[50000], tarray->current[50000], tarray->next[50000]);
         /*
         * After each timestep, you should swap the buffers around. Watch out none
         * of the threads actually use the buffers at that time.
         */
         
-        update_rotation(&tarray->old, &tarray->current, &tarray->next);
-        
-        pthread_cond_signal(& start_line);
+        //update_rotation(&tarray.old, &tarray.current, &tarray.next);
+		update_rota(tarray);
+        pthread_mutex_unlock( &count_mutex);
+        pthread_cond_signal(&start_line);
         /*tempray = *old_array;
         *old_array = *current_array;
         *current_array = *next_array;
         *next_array = tempray;*/
     }
+	tarray->boole = 0;
+	
+	
+	
+	for(tc = 0; tc < num_threads; tc++) {
+            pthread_join(thread_ids[tc], NULL);
+    }
+	current_array = tarray->current;
+	printf("%f\n", current_array[50000]); 
+	free(tarray);
+	
     /* You should return a pointer to the array with the final results. */
     return current_array;
 }
