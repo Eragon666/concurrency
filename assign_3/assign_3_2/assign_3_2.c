a1d122c7654c27adabfd589fc818ec612c54d81d
matthijs.klijn@student.uva.nl <cpp1414@fs2.cm.cluster>
2014-11-23 17:37:16 +0100

    Assign3.2 completed -Matthijs

--- assign_3/assign_3_2/assign_3_2.c
+++ assign_3/assign_3_2/assign_3_2.c
@@ -1,24 +1,72 @@
-#include <stddio.h>
+#include <stdio.h>
 #include <string.h>
 #include "mpi.h"
 
+int MYMPI_Bcast(void *buffer, int count, MPI_Datatype datatype, int root, MPI_Comm communicator)
+{
+	int rank, size, tag, next, last = 0;
+		MPI_Status status;
+
+	MPI_Comm_rank(communicator, &rank); // task id
+	MPI_Comm_size(communicator, &size); //size
+
+	//If the rank is root, start sending to 'next'. Else receive from 'last' and send to 'next'
+	if (rank == root) {
+		next = (rank + 1) % size;
+		MPI_Send(buffer, count, datatype, next, tag, communicator);
+	} else {
+		last = mod((rank - 1), size);
+		next = (rank + 1) % size;
+		MPI_Recv(buffer, count, datatype, last, tag, communicator, &status);
+		MPI_Send(buffer, count, datatype, next, tag, communicator);
+	}
+
+	return MPI_SUCCESS;
+}
+
+//Because % in C calculates the remainder it does not work with negative numbers.
+int mod(int a, int b)
+{
+    int r = a % b;
+    return r < 0 ? r + b : r;
+}
+
 int main(int argc, char **argv)
 {
-	char message[20];
-	int i, rank, size;
-	MPI_Status status;
-	int root = 0;
+	char buffer[32] = {0}; //Buffer
+	int i, rank, size, conn, sender;
+	int messagesize = 32;
+	const int root = 4;
 
-	MPI_Init(&argc, &argv);
-	MPI_comm_size(MPI_COMM_WORLD, $size);
-	MPI_Comm_rank(MPI_COMM_WORLD, $rank);
+	//Setup MPI, if an error occured exit.
+	conn = MPI_Init(&argc, &argv);
+	if (conn != MPI_SUCCESS) {
+		fprintf(stderr, "Unable to set up MPI\n");
+		MPI_Abort(MPI_COMM_WORLD, conn);
+	}
 
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	//Only the root will broadcast a message
 	if (rank == root)
 	{
-		strcpy(message, "Hello, world");
+		strcpy(buffer, "Hello, world");
+		messagesize = strlen(buffer) + 1;
+		printf("I (task %d of %d total tasks) will broadcast: %s\n", rank, size, buffer);
 	}
-	MPI_Bcast(message, 13, MPI_CHAR, root, MPI_COMM_WORLD);
-	printf( "Message from process %d : %s\n", rank, message);
+
+	//Test to check if the buffer is really empty before receiving anything
+	/*printf("My (%d) buffer contains %d characters (%s)\n", rank, strlen(buffer), buffer);*/
+
+	//The root will send it's buffer the other processes will only receive.
+	MYMPI_Bcast(&buffer, messagesize, MPI_CHAR, root, MPI_COMM_WORLD);
+
+	//If not root, print the received message
+	if (rank != root) {
+		sender = mod((rank - 1), size);
+		printf("Hi! I (task: %d) just received '%s' from task %d this message was broadcasted by %d\n", rank, buffer, sender, root);
+	}
 
 	MPI_Finalize();
 }
